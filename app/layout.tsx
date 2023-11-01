8240b79dda06e4acb703c29eeb10bbaa0d1fad65
kento-yoshida <kento-yoshida@kke.co.jp>
2023-11-01 23:58:54 +0000

    margin auto (centered)

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
-      <body className={inter.className}>
+      <body className={`${inter.className} m-auto`}>
         <Header/>
         {children}
         <Analytics />
