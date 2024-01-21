04f451949e4d2c2856baaf107cb59a457b99ff9d
kento-yoshida <kento-yoshida@kke.co.jp>
2024-01-21 23:52:58 +0000

    Avoid empty search requests

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -3,6 +3,42 @@ import Link from 'next/link';
 import algoliasearch from 'algoliasearch/lite';
 import { InstantSearch, SearchBox, Hits } from 'react-instantsearch';
 import type { AlgoliaHitProps } from '@/utils/types';
+import {
+  MultipleQueriesResponse,
+  MultipleQueriesQuery,
+} from "@algolia/client-search";
+import type { SearchClient } from "instantsearch.js";
+
+const algoliaClient = algoliasearch(
+  process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID || '',
+  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
+);
+const indexName = process.env.NEXT_PUBLIC_ALGOLIA_PRIMARY_INDEX || '';
+
+const searchClient: SearchClient = {
+  ...algoliaClient,
+  search: <SearchResponse,>(requests: Readonly<MultipleQueriesQuery[]>) => {
+    if (requests.every(({ params }: MultipleQueriesQuery) => {
+      return !params?.query || params.query.length < 2
+    })
+    ) {
+      return Promise.resolve<MultipleQueriesResponse<SearchResponse>>({
+        results: requests.map(() => ({
+          hits: [],
+          nbHits: 0,
+          nbPages: 0,
+          page: 0,
+          processingTimeMS: 0,
+          hitsPerPage: 0,
+          exhaustiveNbHits: false,
+          query: '',
+          params: '',
+        })),
+      });
+    }
+    return algoliaClient.search(requests);
+  },
+};
 
 function Hit({ hit }: AlgoliaHitProps): JSX.Element {
   const url = `/posts/${hit.objectID}`;
@@ -14,12 +50,6 @@ function Hit({ hit }: AlgoliaHitProps): JSX.Element {
 }
 
 export default function Search(): JSX.Element {
-  const searchClient = algoliasearch(
-    process.env.NEXT_PUBLIC_ALGOLIA_APPLICATION_ID || '',
-    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY || '',
-  );
-  const indexName = process.env.NEXT_PUBLIC_ALGOLIA_PRIMARY_INDEX || '';
-
   return (
     <div>
       <InstantSearch indexName={indexName} searchClient={searchClient}>
